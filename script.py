aaa616b4e59f387c8fe612439b35767835a1bc05
Alex Fantini <fantinialex.2002@gmail.com>
2022-02-16 11:09:21 +0100

    Added table creation, db connection control and some comments

--- script.py
+++ script.py
@@ -3,8 +3,9 @@ import pymysql
 import os
 import time
 
-print("Starting...")
+print('Starting...')
 
+#connect to db
 db = pymysql.connect(
     host=os.environ['DB_HOST'],
     port=int(os.environ['DB_PORT']),
@@ -14,28 +15,51 @@ db = pymysql.connect(
 )
 dbCurson = db.cursor()
 
-while 1:
-    print("Start speedtest")
-    s = speedtest.Speedtest()
-    s.get_servers()
-    s.get_best_server()
+#check connection
+if dbCurson.connection:
+    #check if table exist
+    if dbCurson.execute("SHOW TABLES LIKE 'Speedtest'") != 1:
+        try:
+            dbCurson.execute("CREATE TABLE `Speedtest` (\
+                `id` int(11) unsigned NOT NULL AUTO_INCREMENT,\
+                `isp` text,\
+                `download` float,\
+                `upload` float,\
+                `date` date,\
+                `time` time,\
+                PRIMARY KEY (`id`)\
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8")
+            db.commit()
+            print("Created table 'Speedtest'")
+        except pymysql.Error as e:
+            print("Error %d creating table 'Speedtest': %s" % (e.args[0], e.args[1]))
+            exit(-1)
 
-    isp = s.config['client']['isp']
+    while 1:
+        print("Starting speedtest...")
+        s = speedtest.Speedtest()
+        s.get_servers()
+        s.get_best_server()
 
-    download = s.download()
-    download /= pow(10,6) #convert to mbits/s
-    download = round(download, 2) #round
+        isp = s.config['client']['isp']
 
-    upload = s.upload()
-    upload /= pow(10,6) #convert to mbits/s
-    upload = round(upload, 2) #round
+        download = s.download()
+        download /= pow(10,6) #convert to mbits/s
+        download = round(download, 2) #round
 
-    print("Current connection: "+isp+"\nDownload: "+str(download)+"\nUpload: "+str(upload))
+        upload = s.upload()
+        upload /= pow(10,6) #convert to mbits/s
+        upload = round(upload, 2) #round
 
-    dbCurson.execute("INSERT INTO Speedtest(isp,download,upload,date,time) VALUES ('%s', '%f', '%f', CURDATE(), CURTIME())" % (isp,download,upload))
-    db.commit()
+        print("Current connection: "+isp+"\nDownload: "+str(download)+" Mbps\nUpload: "+str(upload)+" Mbps")
 
-    print("Sleep for "+os.environ['TIME'])
-    time.sleep(int(os.environ['TIME']))
+        dbCurson.execute("INSERT INTO Speedtest(isp,download,upload,date,time) VALUES ('%s', '%f', '%f', CURDATE(), CURTIME())" % (isp,download,upload))
+        db.commit()
 
-db.close()
+        print("Sleep for "+os.environ['TIME']+"s")
+        time.sleep(int(os.environ['TIME']))
+
+    db.close()
+
+else:
+    print("Error connecting to db")
